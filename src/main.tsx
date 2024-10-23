374da5613e16ceb7e55486a47cc9abce3b16e685
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-10-23 17:23:03 -0300

    refactor: 🔧 add the recoil root component to the `main` file

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { router } from "@/router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
   </StrictMode>
 );
