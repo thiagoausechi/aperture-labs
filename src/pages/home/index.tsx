fe287ffc90a9e39ffcc597b95cff28d4b19ccce2
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-10-23 18:43:31 -0300

    feat: ✨ add marketing information to the `hero` page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -22,6 +22,23 @@ export const Home = () => {
           />
         </Suspense>
       </Canvas>
+
+      <section className="col-span-4 col-start-2 self-center">
+        <h1 className="font-black text-6xl uppercase">
+          Junte-se à Revolução Científica
+        </h1>
+        <h3 className="font-medium text-md text-gray-500">
+          Participe da nossa missão de redefinir o futuro com tecnologias
+          revolucionárias e experimentos de ponta.
+        </h3>
+
+        <p className="text-md mt-11 font-medium">
+          Na Aperture Science, acreditamos que o impossível é apenas mais uma
+          barreira a ser superada. Venha fazer parte de uma equipe dedicada à
+          inovação, onde o próximo grande avanço depende da sua criatividade e
+          coragem para testar o desconhecido.
+        </p>
+      </section>
     </Fragment>
   );
 };
