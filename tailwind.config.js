f92b3807b0e4dfbd5e1daeae31c75e9e2e989f85
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-10-23 17:19:55 -0300

    chore: 🔧 add a new gradient color `portal` to the tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
       center: true,
       padding: "1.5rem",
     },
-    extend: {},
+    extend: {
+      colors: {
+        portal:
+          "linear-gradient(90deg, rgba(27,92,182,1) 0%, rgba(92,157,196,1) 33%, rgba(214,154,108,1) 67%, rgba(204,129,58,1) 100%)",
+      },
+    },
   },
   plugins: [],
 };
